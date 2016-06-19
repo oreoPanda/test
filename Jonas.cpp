92be42832e2cf26e0e36575f1d2e899f12f7c0e5
Jonas <legojonas@gmx.de>
2016-06-19 15:54:31 +0200

    Added gitignore and Jonas's code

---
+++ Jonas.cpp
@@ -0,0 +1,21 @@
+/*
+ * Jonas.cpp
+ *
+ *  Created on: Jun 19, 2016
+ *      Author: i_fly_gliders
+ */
+
+#include "Jonas.h"
+
+namespace namesss {
+
+Jonas::Jonas() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Jonas::~Jonas() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace namesss */
---
+++ Jonas.h
@@ -0,0 +1,21 @@
+/*
+ * Jonas.h
+ *
+ *  Created on: Jun 19, 2016
+ *      Author: i_fly_gliders
+ */
+
+#ifndef JONAS_H_
+#define JONAS_H_
+
+namespace namesss {
+
+class Jonas {
+public:
+	Jonas();
+	virtual ~Jonas();
+};
+
+} /* namespace namesss */
+
+#endif /* JONAS_H_ */
